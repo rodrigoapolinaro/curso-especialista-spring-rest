d8e11cc7efc843090dd53c8e50c1ebcc1d90e6c6
rodrigoapolinaro <rodrigoapolinaro@hotmail.com>
2019-09-30 22:59:49 -0300

    2.16. Ambiguidade de beans e injeção de lista de beans

---
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notficacao/NotificarSMS.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.di.notficacao;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+@Component
+public class NotificarSMS implements Notificador {
+	
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		
+		System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
+				cliente.getNome(), cliente.getTelefone(), mensagem);
+	}
+
+}
