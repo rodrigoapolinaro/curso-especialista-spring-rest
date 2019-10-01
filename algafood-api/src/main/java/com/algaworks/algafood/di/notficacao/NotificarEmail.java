229c7d0e7c59db2af0a385b88dc3b75198ca2867
rodrigoapolinaro <rodrigoapolinaro@hotmail.com>
2019-09-30 22:29:53 -0300

    2.14. Conhecendo os pontos de injeção e a anotação @Autowired

--- algafood-api/src/main/java/com/algaworks/algafood/di/notficacao/NotificarEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notficacao/NotificarEmail.java
@@ -1,30 +1,17 @@
 package com.algaworks.algafood.di.notficacao;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 
+@Component
 public class NotificarEmail implements Notificador {
-	
-	private boolean caixaAlta;
-	private String hostServidorSmtp;
-	
-	
-	public NotificarEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println("NotificadorEmail");
-	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		if (this.caixaAlta) {
-			mensagem = mensagem.toUpperCase();
-		}
 		
-		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n",
-				cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
-	}
-	
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
+		System.out.printf("Notificando %s através do e-mail %s: %s\n",
+				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
 }
