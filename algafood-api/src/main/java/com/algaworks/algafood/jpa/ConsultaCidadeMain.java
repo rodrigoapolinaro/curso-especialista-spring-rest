c162cea1f0fb33eca2310b9ef7ef40de01ee1b51
rodrigoapolinaro <rodrigoapolinaro@hotmail.com>
2019-10-15 20:46:30 -0300

    3.20. Desafio: mapeando entidades

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/ConsultaCidadeMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/ConsultaCidadeMain.java
@@ -1,26 +1,30 @@
 package com.algaworks.algafood.jpa;
 
+import java.util.List;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
-import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.repository.CidadeRepository;
 
-public class ExclusaoRestauranteMain {
+public class ConsultaCidadeMain {
 	
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
 				.web(WebApplicationType.NONE)
 				.run(args);
 		
-		RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
+		CidadeRepository cidadeRepository = applicationContext.getBean(CidadeRepository.class);
 		
-		Restaurante restaurante = new Restaurante();
-		restaurante.setId(2L);
+		List<Cidade> cidades = cidadeRepository.listar();
 		
-		restauranteRepository.remover(restaurante);
+		for (Cidade cidade : cidades) {
+			System.out.println(cidade.getNome());
+		}
+		
 	}
 
 }
