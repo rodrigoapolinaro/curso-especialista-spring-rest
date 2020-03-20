f03800a8116b2eecea6df941dd6e40d3c42c6fd9
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-03-20 01:57:00 -0300

    6.14. Resolvendo o Problema do N+1 com fetch join na JPQL

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -63,8 +63,8 @@ public class Restaurante {
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
 	
-	@JsonIgnore
-	@ManyToMany(fetch = FetchType.EAGER)
+//	@JsonIgnore
+	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento",
 			joinColumns = @JoinColumn(name = "restaurante_id"),
 			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
