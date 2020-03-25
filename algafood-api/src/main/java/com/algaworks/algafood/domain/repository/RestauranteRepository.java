b3625062f847bd9abbfd4477c5ba6729128be624
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-03-25 13:55:38 -0300

    7.11. Reparando migrações com erros

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -17,7 +17,7 @@ public interface RestauranteRepository
 		JpaSpecificationExecutor<Restaurante> {
 	
 	
-	@Query("from Restaurante r join fetch r.cozinha join fetch r.formasPagamento")
+	@Query("from Restaurante r join fetch r.cozinha")
 	List<Restaurante> findAll();
 	
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
