75501aa422bf136123534705ff0bdeaa4e15da7a
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-03-31 01:33:12 -0300

    8.5. Simplificando o código com o uso de @ResponseStatus em exceptions

--- algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontraException.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontraException.java
@@ -1,19 +1,15 @@
 package com.algaworks.algafood.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-//@ResponseStatus(value = HttpStatus.NOT_FOUND) //, reason = "Entidade não encontrada")
-public class EntidadeNaoEncontraException extends ResponseStatusException {
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class EntidadeNaoEncontraException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
-	public EntidadeNaoEncontraException(HttpStatus status, String mensagem) {
-		super(status, mensagem);
-	}
 
 	public EntidadeNaoEncontraException(String mensagem) {
-		this(HttpStatus.NOT_FOUND, mensagem);
+		super(mensagem);
 	}
 
 }
--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -3,9 +3,7 @@ package com.algaworks.algafood.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontraException;
@@ -14,6 +12,12 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @Service
 public class CadastroCozinhaService {
+	
+	private static final String MSG_COZINHA_EM_USO 
+			= "Cozinha de código %d não pode ser removida, pois está em uso";
+
+	private static final String MSG_COZINHA_NAO_ENCONTRADA 
+			= "Não existe um cadastro de cozinha com código %d";
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
@@ -28,11 +32,17 @@ public class CadastroCozinhaService {
 			
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontraException(
-					String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
+					String.format(MSG_COZINHA_NAO_ENCONTRADA, cozinhaId));
 									
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
+					String.format(MSG_COZINHA_EM_USO, cozinhaId));
 		}
+	}
+	
+	public Cozinha buscarOuFalhar(Long cozinhaId) {
+		return cozinhaRepository.findById(cozinhaId)
+				.orElseThrow(() -> new EntidadeNaoEncontraException(
+						String.format(MSG_COZINHA_NAO_ENCONTRADA, cozinhaId)));	
 	}
 }
