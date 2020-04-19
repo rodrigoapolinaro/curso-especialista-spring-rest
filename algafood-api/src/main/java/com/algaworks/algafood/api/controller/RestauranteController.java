988848a5da4ba3a51944fba66f4b19a8f963f158
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-04-19 01:00:17 -0300

    8.24. Lançando exception de desserialização na atualização parcial (PATCH)

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,9 +4,14 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +28,7 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -72,7 +78,7 @@ public class RestauranteController {
 	
 	@PatchMapping("/{restauranteId}")
 	public Restaurante atualizarParcial(@PathVariable Long restauranteId,
-			@RequestBody Map<String, Object> campos) {
+			@RequestBody Map<String, Object> campos, HttpServletRequest request) {
 		
 		campos.forEach((nomePropriedade, valorPropriedade) -> {
 			System.out.println(nomePropriedade + " = " + valorPropriedade);
@@ -80,7 +86,7 @@ public class RestauranteController {
 		
 		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		
-		merge(campos, restauranteAtual);
+		merge(campos, restauranteAtual, request);
 		
 		return atualizar(restauranteId, restauranteAtual);
 	}
@@ -90,25 +96,35 @@ public class RestauranteController {
 		chamando métodos alterando valores de atributos tudo isso em tempo de execução de forma dinâmica
 		quando não sabemos nomes de métodos, nomes das propriedades queremos fazer algo mais genérico.
 	*/
-
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		// Responsável por converter serializar objeto java em json/ json em objeto java
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino,
+			HttpServletRequest request) {
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
 		
-		System.out.println(restauranteOrigem);
+		try { 
+			// Responsável por converter serializar objeto java em json/ json em objeto java
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 		
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			// Field busca uma variável de instância
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			field.setAccessible(true); // Tornando a variável acessível
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 			
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			System.out.println(restauranteOrigem);
 			
-			System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
-			
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				// Field busca uma variável de instância
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true); // Tornando a variável acessível
+				
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+				
+				System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
+				
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+		}
 		
 	}
 
