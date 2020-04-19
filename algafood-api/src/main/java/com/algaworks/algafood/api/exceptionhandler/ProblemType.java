f5f0fe09a3aa66692a1d3cfd256cb3ac2cbebb05
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-04-19 15:54:03 -0300

    8.25. Desafio: tratando exception de parâmetro de URL inválido

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
