39a62fc559b5fa34c85fd57bdf6a87d04d0127cb
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-04-28 00:36:02 -0300

    9.9. Desafio: adicionando constraints de validação no modelo

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -36,7 +38,7 @@ public class EstadoController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Estado adicionar(@RequestBody Estado estado) {
+	public Estado adicionar(@RequestBody @Valid Estado estado) {
 		return cadastroEstado.salvar(estado);
 	}
 	
@@ -48,7 +50,7 @@ public class EstadoController {
 	
 	@PutMapping("/{estadoId}")
 	public Estado atualizar(@PathVariable Long estadoId,
-			@RequestBody Estado estado) {
+			@RequestBody @Valid Estado estado) {
 		
 		Estado estadoAtual = cadastroEstado.buscarOuFalhar(estadoId);
 			
