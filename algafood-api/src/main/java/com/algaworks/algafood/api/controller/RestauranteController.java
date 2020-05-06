89409961676613202b2c18ad579852d05a3bf2f0
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-05-06 00:19:15 -0300

    9.20. Desafio: tratando a exception customizada de validações programáticas

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.core.validation.ValidacaoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontraException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -104,7 +105,7 @@ public class RestauranteController {
 		validator.validate(restaurante, bindingResult);
 		
 		if (bindingResult.hasErrors()) {
-			
+			throw new ValidacaoException(bindingResult);
 		}
 	}
 
