17dd7bdc579b06180f12181b5d877f85fa77a212
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-05-14 00:49:08 -0300

    11.3. Criando classes de mixin para usar as anotações do Jackson

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/mixin/RestauranteMixin.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.api.model.mixin;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Endereco;
+import com.algaworks.algafood.domain.model.FormaPagamento;
+import com.algaworks.algafood.domain.model.Produto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class RestauranteMixin {
+	
+	@JsonIgnoreProperties(value = "nome", allowGetters = true)
+	private Cozinha cozinha;
+	
+	@JsonIgnore
+	private Endereco endereco;
+	
+	@JsonIgnore
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCadastro;
+	
+	@JsonIgnore
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataAtualizacao;
+	
+	@JsonIgnore
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+	
+	@JsonIgnore
+	private List<Produto> produtos = new ArrayList<>();
+
+}
