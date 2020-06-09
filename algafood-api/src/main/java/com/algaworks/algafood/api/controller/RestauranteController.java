be6e1b5188bd3d16378c0ccfd87452ac638ef8ad
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-06-09 00:07:25 -0300

    12.4. Implementando os endpoints de ativação e inativação de restaurantes

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,21 +69,28 @@ public class RestauranteController {
 	@PutMapping("/{restauranteId}")
 	public RestauranteModel atualizar(@PathVariable Long restauranteId,
 		@RequestBody @Valid RestauranteInput restauranteInput) {
-		try { 
-//			Restaurante restaurante = restauranteInputDisassembler.toDomainObject(restauranteInput);
-			
+		try { 			
 			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 			
-			restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);
-
-//			BeanUtils.copyProperties(restaurante, restauranteAtual, 
-//					"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
-		
+			restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);	
 		
 			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
 		} catch(EntidadeNaoEncontraException e) {
 			throw new NegocioException(e.getMessage());
 		}
 	}	
-
+	
+	@PutMapping("/{restauranteId}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void ativar(@PathVariable Long restauranteId) {
+		cadastroRestaurante.ativar(restauranteId);
+	}
+	
+	@DeleteMapping("/{restauranteId}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void inativar(@PathVariable Long restauranteId) {
+		cadastroRestaurante.inativar(restauranteId);
+	}
+	
+	
 }
