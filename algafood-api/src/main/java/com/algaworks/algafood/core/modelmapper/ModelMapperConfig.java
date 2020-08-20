04beb114f70b957c5353b46897647f9efa67e75e
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-08-20 01:01:22 -0300

    12.21. Desafio: Implementando o endpoint de emissão de pedidos

--- algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -5,26 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.api.model.input.ItemPedidoInput;
 import com.algaworks.algafood.domain.model.Endereco;
+import com.algaworks.algafood.domain.model.ItemPedido;
 
 @Configuration
 public class ModelMapperConfig {
 	
 	@Bean
 	public ModelMapper modelMapper() {
-		var modelmapper = new ModelMapper();
+		var modelMapper = new ModelMapper();
 		
-//		modelmapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
 //			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
 		
-		var enderecoToEnderecoModelTypeMap = modelmapper.createTypeMap(
+		modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+			.addMappings(mapper -> mapper.skip(ItemPedido::setId));
+		
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
 				Endereco.class, EnderecoModel.class);
 		
 		enderecoToEnderecoModelTypeMap.<String>addMapping(
 				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
 				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
 		
-		return modelmapper;
+		return modelMapper;
 	}
 
 }
