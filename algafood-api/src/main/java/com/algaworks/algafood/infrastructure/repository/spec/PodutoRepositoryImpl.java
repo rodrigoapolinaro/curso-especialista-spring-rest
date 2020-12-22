f00a011729db8e1d24c6594f45b87e111b37c098
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-12-22 12:07:19 -0300

    14.6. Implementando serviço de cadastro de foto de produto

---
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/spec/PodutoRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.infrastructure.repository.spec;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.repository.ProdutoRepositoryQueries;
+
+@Repository
+public class PodutoRepositoryImpl implements ProdutoRepositoryQueries {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Transactional
+	@Override
+	public FotoProduto save(FotoProduto foto) {
+		return manager.merge(foto);
+	}
+
+}
