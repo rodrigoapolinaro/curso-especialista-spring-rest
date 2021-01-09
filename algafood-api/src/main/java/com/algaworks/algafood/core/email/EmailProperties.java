983255aefca9fae5f053059a01af774beededd10
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-01-09 00:23:51 -0300

    15.8. Desafio: implementando serviço de envio de e-mail fake

--- algafood-api/src/main/java/com/algaworks/algafood/core/email/EmailProperties.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/email/EmailProperties.java
@@ -15,8 +15,17 @@ import lombok.Setter;
 @Component
 @ConfigurationProperties("algafood.email")
 public class EmailProperties {
-
+	
+	//Atribuimos FAKE como padrão
+	//Isso evita o problema de enviar e-mails de verdade caso você esqueça
+	//de definir a propriedade
+	private Implementacao impl = Implementacao.FAKE;
+	
 	@NotNull
 	private String remetente;
+	
+	public enum Implementacao {
+		SMTP, FAKE
+	}
 	
 }
