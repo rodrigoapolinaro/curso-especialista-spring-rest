1bbc6e53afa50b35835e04b693c00474a1cc3fd6
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-01-17 14:32:13 -0300

    18.6. Descrevendo informações da API na documentação

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,8 +24,17 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
 					.paths(PathSelectors.any())
-//					.paths(PathSelectors.ant("/restaurantes/*"))
-					.build();
+					.build()
+				.apiInfo(apiInfo());
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Algafood API")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("Algaworks", "https://www.algaworks.com", "contato@algaworks.com"))
+				.build();
 	}
 	
 	@Override
@@ -31,7 +43,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 			.addResourceLocations("classpath:/META-INF/resources/");
 		
 		registry.addResourceHandler("/webjars/**")
-			.addResourceLocations("classpath:/META-INF/resource/webjars/");
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
