4a01ba815334896df9643642ff17be443cbae916
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-01-18 22:43:30 -0300

    18.36. Corrigindo documentação no Swagger UI para upload de arquivos

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -53,10 +54,11 @@ public class RestauranteProdutoFotoController implements RestauranteProdutoFotoC
 	
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
+			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput,
+			@RequestParam(required = true) MultipartFile arquivo) throws IOException {
 		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 		
-		MultipartFile arquivo = fotoProdutoInput.getArquivo();
+//		MultipartFile arquivo = fotoProdutoInput.getArquivo();
 
 		FotoProduto foto = new FotoProduto();
 		foto.setProduto(produto);
