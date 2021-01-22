1af469aa45c31416f1d3931d1d0dd95cf4329a57
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-01-21 23:55:51 -0300

    19.21. Desafio: refatorando construção e inclusão de links

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/CidadeModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/CidadeModelAssembler.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.api.assembler;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,8 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.controller.CidadeController;
-import com.algaworks.algafood.api.controller.EstadoController;
 import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.domain.model.Cidade;
 
@@ -21,6 +20,9 @@ extends RepresentationModelAssemblerSupport<Cidade, CidadeModel> {
 	@Autowired
 	private ModelMapper modelMapper;
 	
+	@Autowired
+	private AlgaLinks algaLinks;
+	
 	public CidadeModelAssembler() {
 		super(CidadeController.class, CidadeModel.class);
 	}
@@ -28,14 +30,11 @@ extends RepresentationModelAssemblerSupport<Cidade, CidadeModel> {
 	@Override
 	public CidadeModel toModel(Cidade cidade) {
 		CidadeModel cidadeModel = createModelWithId(cidade.getId(), cidade);
-		
 		modelMapper.map(cidade, cidadeModel);
 		
-		cidadeModel.add(linkTo(methodOn(CidadeController.class)
-				.listar()).withRel("cidades"));
+		cidadeModel.add(algaLinks.linkToCidades("cidades"));
 		
-		cidadeModel.getEstado().add(linkTo(methodOn(EstadoController.class)
-				.buscar(cidadeModel.getEstado().getId())).withSelfRel());
+		cidadeModel.getEstado().add(algaLinks.linkToEstado(cidadeModel.getEstado().getId()));
 				
 		return cidadeModel;
 	}
--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/UsuarioModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/UsuarioModelAssembler.java
@@ -1,7 +1,6 @@
 package com.algaworks.algafood.api.assembler;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,8 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.AlgaLinks;
 import com.algaworks.algafood.api.controller.UsuarioController;
-import com.algaworks.algafood.api.controller.UsuarioGrupoController;
 import com.algaworks.algafood.api.model.UsuarioModel;
 import com.algaworks.algafood.domain.model.Usuario;
 
@@ -22,19 +21,20 @@ public class UsuarioModelAssembler
 	@Autowired
 	private ModelMapper modelMapper;
 	
+	@Autowired
+	private AlgaLinks algaLinks;
+	
 	public UsuarioModelAssembler() {
 		super(UsuarioController.class, UsuarioModel.class);
 	}
 	
 	public UsuarioModel toModel(Usuario usuario) {
 		UsuarioModel usuarioModel = createModelWithId(usuario.getId(), usuario);
-		
 		modelMapper.map(usuario, usuarioModel);
 		
-		usuarioModel.add(linkTo(UsuarioController.class).withRel("usuarios"));
+		usuarioModel.add(algaLinks.linkToUsuarios("usuarios"));
 		
-		usuarioModel.add(linkTo(methodOn(UsuarioGrupoController.class)
-				.listar(usuario.getId())).withRel("grupos-usuarios"));
+		usuarioModel.add(algaLinks.linkToGruposUsuario(usuario.getId(), "grupos-usuarios"));
 		
 		return usuarioModel;
 	}
