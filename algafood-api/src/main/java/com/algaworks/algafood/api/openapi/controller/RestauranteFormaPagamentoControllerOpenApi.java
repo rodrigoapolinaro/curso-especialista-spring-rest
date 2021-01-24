13b33cf1145854b8f533407baa7e7bd145169f38
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-01-24 10:55:14 -0300

    19.28. Adicionando links para desassociação de formas de pagamento com restaurante

--- algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenApi.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.openapi.controller;
 
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
@@ -28,7 +29,7 @@ public interface RestauranteFormaPagamentoControllerOpenApi {
         @ApiResponse(code = 404, message = "Restaurante ou forma de pagamento não encontrado", 
             response = Problem.class)
     })
-	public void desassociar(
+	public ResponseEntity<Void> desassociar(
             @ApiParam(value = "ID do restaurante", example = "1", required = true)
 			Long restauranteId, 
             @ApiParam(value = "ID da forma de pagamento", example = "1", required = true)
