2d6bb1742748f96886c71df3c68662f30d4063f1
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-01-24 00:24:45 -0300

    19.26. Desafio: adicionando template variable de projeção de restaurantes

--- algafood-api/src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -6,10 +6,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariable.VariableType;
 import org.springframework.hateoas.TemplateVariables;
 import org.springframework.hateoas.UriTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.hateoas.TemplateVariable.VariableType;
 
 import com.algaworks.algafood.api.controller.CidadeController;
 import com.algaworks.algafood.api.controller.CozinhaController;
@@ -32,7 +32,10 @@ public class AlgaLinks {
 			new TemplateVariable("size", VariableType.REQUEST_PARAM),
 			new TemplateVariable("sort", VariableType.REQUEST_PARAM));
 	
-	public Link linkToPedidos() {		
+	public static final TemplateVariables PROJECAO_VARIABLES = new TemplateVariables(
+			new TemplateVariable("projecao", VariableType.REQUEST_PARAM));
+	
+	public Link linkToPedidos(String rel) {		
 		TemplateVariables filtroVariables = new TemplateVariables(
 				new TemplateVariable("clienteId",VariableType.REQUEST_PARAM),
 				new TemplateVariable("restauranteId",VariableType.REQUEST_PARAM),
@@ -42,7 +45,7 @@ public class AlgaLinks {
 		String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
 		
 		return new Link(UriTemplate.of(pedidosUrl, 
-				PAGINACAO_VARIABLES.concat(filtroVariables)), "pedidos");
+				PAGINACAO_VARIABLES.concat(filtroVariables)), rel);
 	}
 	
 	public Link linkToConfirmacaoPedido(String codigoPedido, String rel) {
@@ -70,7 +73,9 @@ public class AlgaLinks {
 	}
 	
 	public Link linkToRestaurantes(String rel) {
-		return linkTo(RestauranteController.class).withRel(rel);
+		String restaurantesUrl = linkTo(RestauranteController.class).toUri().toString();
+		
+		return new Link(UriTemplate.of(restaurantesUrl, PROJECAO_VARIABLES), rel);
 	}
 	
 	public Link linkToRestaurantes() {
