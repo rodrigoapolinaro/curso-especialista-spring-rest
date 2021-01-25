81f69f50f83f77e77a2825954d269c54be016ea4
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-01-24 23:30:34 -0300

    19.37. Desafio: implementando endpoint com links de recursos de estatísticas

--- algafood-api/src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.api.controller.CidadeController;
 import com.algaworks.algafood.api.controller.CozinhaController;
 import com.algaworks.algafood.api.controller.EstadoController;
+import com.algaworks.algafood.api.controller.EstatisticasController;
 import com.algaworks.algafood.api.controller.FluxoPedidoController;
 import com.algaworks.algafood.api.controller.FormaPagamentoController;
 import com.algaworks.algafood.api.controller.GrupoController;
@@ -50,6 +51,23 @@ public class AlgaLinks {
 		
 		return new Link(UriTemplate.of(pedidosUrl, 
 				PAGINACAO_VARIABLES.concat(filtroVariables)), rel);
+	}
+	
+	public Link linkToEstatisticasVendasDiarias(String rel) {
+	    TemplateVariables filtroVariables = new TemplateVariables(
+	            new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("timeOffset", VariableType.REQUEST_PARAM));
+	    
+	    String pedidosUrl = linkTo(methodOn(EstatisticasController.class)
+	            .consultarVendasDiarias(null, null)).toUri().toString();
+	    
+	    return new Link(UriTemplate.of(pedidosUrl, filtroVariables), rel);
+	} 
+	
+	public Link linkToEstatisticas(String rel) {
+	    return linkTo(EstatisticasController.class).withRel(rel);
 	}
 	
 	public Link linkToConfirmacaoPedido(String codigoPedido, String rel) {
