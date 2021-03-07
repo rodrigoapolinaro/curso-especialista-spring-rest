8098f2f6aed88ac328e04c64448d76777fe1d0ea
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2021-03-07 18:42:46 -0300

    23.19. Carregando as permissões concedidas na autenticação

--- algafood-api/src/main/resources/db/testdata/afterMigrate.sql
+++ algafood-api/src/main/resources/db/testdata/afterMigrate.sql
@@ -124,7 +124,7 @@ insert into usuario (id, nome, email, senha, data_cadastro) values
 (6, 'Débora Mendonça', 'rodrigoapolinaro@gmail.com', '$2y$12$PQRLP4gjYNY1WBZwLsEmO./K.zPgoYsBVpzbgfmpbnVmvjaLm2mqi', utc_timestamp),
 (7, 'Carlos Lima', 'rodrigoapolinaro.aw+carlos@gmail.com', '$2y$12$PQRLP4gjYNY1WBZwLsEmO./K.zPgoYsBVpzbgfmpbnVmvjaLm2mqi', utc_timestamp);
 
-insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2); 
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2), (3, 3), (4, 4); 
 
 insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values (1, 5), (3, 5); 
 
